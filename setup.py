99448c92035f55dd2f0d6576364dad9e5c9e5a29
Santosh <santosh4thmarch@gmail.com>
2026-01-26 19:41:04 +0530

    Project structure is ready

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='End to end Production grade MLops Project to deploy IEE solution',
+    author='santosh soni',
+    license='',
+)
